adf5504367ecde1edc7ca7bb8cd4f874303c03b1
Oleh Dev <oleh.dev83@gmail.com>
2022-12-24 15:42:15 +0100

    Добавили к задаче styles: rename, autoprefixer и cleanCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
-const gulp          = require('gulp');
-const sass          = require('gulp-sass')(require('sass'));
-const browserSync   = require('browser-sync');
+const gulp        	= require('gulp');
+const sass        	= require('gulp-sass')(require('sass'));
+const rename 		= require("gulp-rename");
+const autoprefixer 	= require('gulp-autoprefixer');
+const cleanCSS 		= require('gulp-clean-css');
+const browserSync 	= require('browser-sync');
 
 // Задача для запуска сервера
 
-gulp.task('server', function(){
-    browserSync.init({
+gulp.task('server', function() {
+
+    browserSync({
         server: {
             baseDir: "src"
         }
@@ -14,19 +18,22 @@ gulp.task('server', function(){
 
 // Задача для компиляции стилей
 
-gulp.task('styles', function(){
-    return gulp.src("src/scss/**/*.+(scss|sass)")
+gulp.task('styles', function() {
+    return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest("scr/scss"))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
 
 // Задача для отслежки изминении в файлах
 
-gulp.task('watch', function(){
-    gulp.watch("src/scss/**/*.+(scss|sass)", gulp.parallel("styles"))
-    gulp.watch("src*.html", browserSync).on("change", browserSync.reload)
-});
+gulp.task('watch', function() {
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+	gulp.watch("src/*.html").on('change', browserSync.reload);
+})
 
 // Запуск задач по умолчанию
 
